dc25eada0162d137720624d28af917dd200b934b
gaelforget <gforget@mit.edu>
2021-09-28 09:21:27 -0400

    another test

--- sysimage/run_notebooks.jl
+++ sysimage/run_notebooks.jl
@@ -9,22 +9,25 @@ function fig1to5(x::ModelConfig)
 
     #1.download
 
-    url="https://dap.ceda.ac.uk/badc/ar6_wg1/data/spm/"
-    #url="https://dap.ceda.ac.uk/badc/ar6_wg1/data/spm/spm_04/v20210809/"
+    #url="https://dap.ceda.ac.uk/badc/ar6_wg1/data/spm/"
+    url="https://dap.ceda.ac.uk/badc/ar6_wg1/data/spm/spm_01/v20210809/"
     pth=joinpath(x.folder,string(x.ID))
     run(`wget -q -e robots=off --mirror --no-parent -r $url`)
 
     #2. run loop over notebooks
 
-    for ii in 1:5
+    for ii in 1:1
         fil_in="notebook_0$(ii).jl"
         cp(joinpath(pth0,"IPCC",fil_in),fil_in)
-        PlutoSliderServer.export_notebook(fil_in)
-        fil_out="notebook_0$(ii).html"
-        isdir(pth01) ? cp(fil_out,joinpath(pth01,fil_out))  : nothing
+        #PlutoSliderServer.export_notebook(fil_in)
+        #fil_out="notebook_0$(ii).html"
+        #isdir(pth01) ? cp(fil_out,joinpath(pth01,fil_out))  : nothing
     end
 
     cd(pth0)
+
+    println.(readdir(pth0))
+    println.(readdir(pth))
 
     return x
 end 
