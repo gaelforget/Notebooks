e650bb11a982ab6ea0c21ce23935f31e827d745a
gaelforget <gforget@mit.edu>
2021-09-28 13:04:39 -0400

    another test

--- IPCC/run_notebooks.jl
+++ IPCC/run_notebooks.jl
@@ -19,7 +19,7 @@ function fig1to5(x::ModelConfig)
     #2. run loop over notebooks
 
     cp(joinpath(pth0,"IPCC","pth_ipcc.jl"),"pth_ipcc.jl")
-    for ii in x.inputs["notebookIDs"]
+    for ii in x.inputs[:notebookIDs]
         fil_in="notebook_0$(ii).jl"
         println("where are we now $i?")
         println(pwd())
