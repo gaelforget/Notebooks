dddb5e9297c95570e5cf7f50151a9cb206d933c7
gaelforget <gforget@mit.edu>
2021-12-14 14:31:48 -0500

    test creating sysimage with fewer pkgs

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -1,7 +1,4 @@
 using PackageCompiler
-create_sysimage([:CSV, :ClimateModels, :DataFrames, 
-                 :IJulia, :IndividualDisplacements, :MITgcmTools, 
-                 :Plots, :Pluto, :PlutoUI, :UnicodePlots, :CairoMakie];
-                precompile_execution_file = "sysimage/warmup.jl",
+create_sysimage([:CSV, :DataFrames],
                 sysimage_path="ExampleSysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
