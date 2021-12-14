cc400c690cdabda61a13f1c182e091469837bc0e
gaelforget <gforget@mit.edu>
2021-12-14 18:45:11 -0500

    try with fewer packages in sysimage

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -1,6 +1,5 @@
 using PackageCompiler
 create_sysimage([:CSV, :DataFrames, :IJulia, :Pluto, :PlutoUI, 
-                :MeshArrays, :ClimateModels, :MITgcmTools, :IndividualDisplacements, 
                 :UnicodePlots, :CairoMakie, :Plots],
                 sysimage_path="ExampleSysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
