9f23d49adf9ca8559027495146692a4d202066fa
gaelforget <gforget@mit.edu>
2021-12-14 18:00:46 -0500

    add packages back to sysimage

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -1,4 +1,6 @@
 using PackageCompiler
-create_sysimage([:CSV, :DataFrames],
+create_sysimage([:CSV, :DataFrames, :IJulia, :Pluto, :PlutoUI, 
+                :MeshArrays, :ClimateModels, :MITgcmTools, :IndividualDisplacements, 
+                :UnicodePlots, :CairoMakie, :Plots],
                 sysimage_path="ExampleSysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
