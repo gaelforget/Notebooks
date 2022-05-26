132ef0794c7a6f2ade252f56485f4f5a04af16b4
gaelforget <gforget@mit.edu>
2022-05-26 10:57:50 -0400

    another try

--- sysimage/create_sysimage.jl
+++ sysimage/create_sysimage.jl
@@ -1,6 +1,5 @@
 using PackageCompiler
-create_sysimage([:CSV, :DataFrames, :IJulia, :Pluto, :PlutoUI, 
-                :MeshArrays, :ClimateModels, :MITgcmTools];
-                precompile_execution_file = "/usr/local/etc/gf/sysimage/warmup.jl",
+create_sysimage([:CSV, :DataFrames, :IJulia, :Pluto, :PlutoUI, :PlutoSliderServer, 
+                 :MeshArrays, :ClimateModels, :MITgcmTools],
                 sysimage_path="/usr/local/etc/gf/ExampleSysimage.so",
                 cpu_target = PackageCompiler.default_app_cpu_target())
